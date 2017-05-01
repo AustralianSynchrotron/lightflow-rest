7ecf1ee40bb9861ccf201e5161d152f30218cb91
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-05-01 10:54:05 +1000

    Implement stop workflow endpoint and return id in start workflow

--- lightflow_rest/api/workflow.py
+++ lightflow_rest/api/workflow.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, current_app, request
 
-from lightflow.workflows import start_workflow
+from lightflow.workflows import start_workflow, stop_workflow
 from lightflow.models.exceptions import (WorkflowArgumentError,
                                          WorkflowImportError)
 
@@ -12,7 +12,7 @@ api = Blueprint('workflow', __name__, url_prefix='/workflow')
 
 @api.route('/<name>', methods=['POST'])
 def api_start_workflow(name):
-    """ Endpoint for starting a new workflow
+    """ Endpoint for starting a new workflow.
     
     The dynamic path variable <name> is the name of the workflow that should be started.
     The endpoint accepts a parameter keep_data (e.g. ?keep_data=1) that tells the system
@@ -30,21 +30,30 @@ def api_start_workflow(name):
         else:
             keep_data = False
 
-        start_workflow(name=name,
-                       config=current_app.config['LIGHTFLOW'],
-                       clear_data_store=not keep_data,
-                       store_args=request.form.to_dict())
+        id = start_workflow(name=name,
+                            config=current_app.config['LIGHTFLOW'],
+                            clear_data_store=not keep_data,
+                            store_args=request.form.to_dict())
 
-        return ApiResponse({'success': True})
+        return ApiResponse({'id': id})
     except (WorkflowArgumentError, WorkflowImportError) as err:
         raise ApiError(StatusCode.InternalServerError, str(err))
-
 
 
 @api.route('/', methods=['DELETE'])
 @api.route('/<name>', methods=['DELETE'])
 def api_stop_workflow(name=None):
-    pass
+    """ Endpoint for stopping all or a single running workflow.
+
+    This endpoint can be called with either a dynamic path variable <name>, specifying
+    the job that should be stopped, or without one which will stop all running workflows.
+    """
+    try:
+        stop_workflow(current_app.config['LIGHTFLOW'],
+                      names=[name] if name is not None else None)
+        return ApiResponse({'success': True})
+    except Exception as err:
+        raise ApiError(StatusCode.InternalServerError, str(err))
 
 
 @api.route('/', methods=['GET'])
