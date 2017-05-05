414c0c36e3afca030a1ff7d1a22cd2604b419558
Andreas Moll <Andreas.Moll@synchrotron.org.au>
2017-05-05 12:57:47 +1000

    Bump up version number of lightflow dependency

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(exclude=['tests', 'examples']),
 
     install_requires=[
-        'lightflow>=1.2.1',
+        'lightflow>=1.2.2',
         'Flask>=0.12.1'
     ],
 )
